821baa156a5330e364e735aec637d5399647430d
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-01-31 14:46:56 -0300

    Adding simpleinjector

---
+++ GraphQLProject/App_Start/IdCConfig.cs
@@ -0,0 +1,20 @@
+﻿using SimpleInjector;
+using System.Web.Http;
+using SimpleInjector.Lifestyles;
+using SimpleInjector.Integration.WebApi;
+using GraphQLProject.App_Start.IdCCustom;
+
+namespace GraphQLProject
+{
+    public class IdCConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            config.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+            config.MessageHandlers.Add(new DelegatingHandlerProxy<InicializeHandler>(container));
+        }
+    }
+}
