9cf7186a08d11f7630d4d241ad618b70173cbf62
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-02-03 17:36:03 -0300

    Show errors api

--- src/GraphQLProject.API/App_Start/WebApiConfig.cs
+++ src/GraphQLProject.API/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace GraphQLProject.API
 {
@@ -19,6 +16,9 @@ namespace GraphQLProject.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Show exceptions
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
         }
     }
 }
